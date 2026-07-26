c5b5064b955396678fc595010dac31b2bacb74b7
Areeba Ali Siddiqui <areebaalisiddiqui@gmail.com>
2026-07-26 10:47:08 +0500

    add upload functionality to the app.py file.

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask
 from flask import render_template
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 
-def indedx():
+def index():
     return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+UPLOAD_FOLDER ='path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('uploaded_file',
+                                        filename=filename))
+        return render_template('index.html')
